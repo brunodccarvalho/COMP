b52abbe072bd9eb00a164ac452b6d1bdba8aa63a
Bruno Carvalho <elbrunovsky@gmail.com>
2019-04-20 00:06:21 +0100

    Starting DiagnosticsHandler

---
+++ src/compiler/DiagnosticsHandler.java
@@ -0,0 +1,27 @@
+package compiler;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class DiagnosticsHandler {
+  private final File file;
+  private final ArrayList<String> lines;
+
+  DiagnosticsHandler(File file) throws IOException {
+    this.file = file;
+    this.lines = new ArrayList<>();
+
+    // Cache file
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        lines.add(line);
+      }
+    }
+  }
+
+  // ... similar logic to ParseException::initialize
+}
